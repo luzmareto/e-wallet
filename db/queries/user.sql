ee75bd2127b4077fed2f1b522891d7f22c0b2381
Luzmareto <riosasa@gmail.com>
2023-05-19 21:14:54 +0700

    [FEATURE] implement user repository

--- db/queries/user.sql
+++ db/queries/user.sql
@@ -8,3 +8,21 @@ INSERT INTO users (
 ) VALUES (
     $1,$2,$3,$4,$5
 ) RETURNINg *;
+
+-- name: DeleteUsers :exec
+DELETE FROM users WHERE id = $1;
+
+-- name: UpdateUsers :one
+UPDATE users SET email = $2, phone_number = $3 WHERE id = $1 RETURNINg *;
+
+-- name: UpdateUsersPassword :exec
+UPDATE users SET password = $2 WHERE id = $1;
+
+-- name: GetUserById :one
+SELECT * FROM users WHERE id = $1;
+
+-- name: GetUserByUserName :one
+SELECT * FROM users WHERE username = $1;
+
+-- name: ListUsers :many
+SELECT * FROM users LIMIT $1 OFFSET $2 ;
