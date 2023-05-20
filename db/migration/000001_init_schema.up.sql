c542ebcd27b377158732c27535e310f49fbfd190
Khilmi Aminudin <khilmiaminudin715@gmail.com>
2023-05-20 12:28:07 +0700

    [fix] fix db schema

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "users" (
   "id" BIGSERIAL PRIMARY KEY,
-  "username" VARCHAR(255) NOT NULL,
+  "username" VARCHAR(255) UNIQUE NOT NULL,
   "password" VARCHAR(255) NOT NULL,
   "email" VARCHAR(255) NOT NULL,
   "phone_number" VARCHAR(20) NOT NULL,
